fda45038f76486ec757ae86306fe29d3e729c7b3
Anton Svärd <antonsvard@antons-macbook-pro-2.local>
2018-11-22 16:51:30 +0100

    Added label and buttons on difficulty page

--- Project/DifficultyViewController.swift
+++ Project/DifficultyViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class DifficultyViewController: UIViewController {
 
+    @IBOutlet weak var easyBtn: UIButton!
+    @IBOutlet weak var mediumBtn: UIButton!
+    @IBOutlet weak var hardBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // navigation bar
         //navigationBarItems()
         
+        easyBtn.backgroundColor = UIColor.white
+        easyBtn.layer.cornerRadius = 20
+        easyBtn.doGlowAnimation(withColor: UIColor.black, withEffect: .big)        
+        mediumBtn.backgroundColor = UIColor.white
+        mediumBtn.layer.cornerRadius = 20
+        mediumBtn.doGlowAnimation(withColor: UIColor.black, withEffect: .big)
+        hardBtn.backgroundColor = UIColor.white
+        hardBtn.layer.cornerRadius = 20
+        hardBtn.doGlowAnimation(withColor: UIColor.black, withEffect: .big)
         // removes navbar bottom line
         self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
 
